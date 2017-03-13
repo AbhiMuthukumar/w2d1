58e81e09ecacf918a144850e55d3d9cba3038d18
Abhi Muthukumar <abhi.muthukumar@gmail.com>
2017-03-13 17:05:59 +0000

    step 4 complete

---
+++ printWithCallbacks.js
@@ -0,0 +1,33 @@
+var https = require("https");
+
+var responseBuffer = '';
+
+function getHTML (options, callback) {
+
+  https.get(options, function(response){
+    response.setEncoding("utf8");
+
+    response.on('data', function(res){
+      responseBuffer += res;
+    });
+
+    response.on('end', function(){
+      callback(responseBuffer);
+    });
+
+  }).on('error', function(err){
+    console.log("Error: " , err);
+  });
+
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+getHTML(requestOptions, printHTML);
