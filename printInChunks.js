a22e50b22f91b48bfbea03a3459c3ae3389e23ee
Abhi Muthukumar <abhi.muthukumar@gmail.com>
2017-03-13 15:42:26 +0000

    step 1 complete

--- printInChunks.js
+++ printInChunks.js
@@ -1,4 +1,4 @@
-var https = require('HTTPS');
+var https = require('https');
 
 function getAndPrintHTMLChunks(){
   var reqOptions = {
@@ -6,5 +6,22 @@ function getAndPrintHTMLChunks(){
   path:'/http-examples/step1.html'
   }
 
+  https.get(reqOptions, function(response){
+    response.setEncoding("utf8");
+
+    response.on('data', function(res){
+      console.log("------------------Length of the chunk received : " + res.length + "------------------");
+      console.log(res);
+    });
+
+    response.on('end', function(){
+      console.log("End of the Response");
+    });
+
+  }).on('error', function(err){
+    console.log("Error: " , err);
+  });
 
 }
+
+getAndPrintHTMLChunks();
